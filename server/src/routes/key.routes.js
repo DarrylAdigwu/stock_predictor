5bec69be0680f013e55a5facddc6cdeecdce8982
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2026-01-05 10:58:49 -0800

    added html, js, and css

--- server/src/routes/key.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getKey } from "../controllers/api.controllers.js";
-
-// Create Router
-export const keyRouter = express.Router();
-
-keyRouter.route("/")
-.get(getKey)
-
