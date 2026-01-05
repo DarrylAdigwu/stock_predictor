fa9f46d56377391bdde92f92662ab640c79063b3
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2026-01-05 11:07:06 -0800

    added openai dependency

---
+++ server/src/controllers/api.controllers.js
@@ -0,0 +1,5 @@
+export const getKey = (async (req, res) => {
+  return res.json({
+    "key": "hi"
+  })
+})
--- server/src/routes/ai.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getResponse } from "../controllers/ai.controllers.js";
-
-// Create Router
-export const aiRouter = express.Router();
-
-aiRouter.route("/")
-.post(getResponse)
-
